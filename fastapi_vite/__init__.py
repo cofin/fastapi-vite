f531fb6c29e152ce4499fa873288b6be9044b232
Cody Fincher <cody.fincher@gmail.com>
2021-06-04 21:02:05 -0500

    adjust extra dependencies

--- fastapi_vite/__init__.py
+++ fastapi_vite/__init__.py
@@ -1,5 +1,5 @@
 from .config import settings
 from .loader import vite_asset, vite_asset_url, vite_hmr_client
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __all__ = [vite_asset_url, vite_hmr_client, vite_asset, settings]
