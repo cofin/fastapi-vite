72a9dc3c5fe99747a04f9b5d40664e9ec3cee43b
Cody Fincher <cody.fincher@gmail.com>
2021-06-27 16:47:55 -0500

    version bump

--- fastapi_vite/__init__.py
+++ fastapi_vite/__init__.py
@@ -1,6 +1,6 @@
 from .config import settings
 from .loader import vite_asset, vite_asset_url, vite_hmr_client
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [vite_asset_url, vite_hmr_client, vite_asset, settings]
